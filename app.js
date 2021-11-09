3f03fa4348f3f5b5e1a0043f262945dff1eec15d
18713 <78613232+18713@users.noreply.github.com>
2021-11-09 13:11:08 -0800

    js start

---
+++ app.js
@@ -0,0 +1,81 @@
+// chapter number 1 ALERT :
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+alert("hello world")
+
+// 2. Write a script to display following message on your web
+// page:
+
+alert("Error! please enter a valid password.")
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+ alert("Welcome to JS Land... \n Happy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+  alert("Welcome to JS Land...")
+  alert("Happy Coding! \n prevent this page from creating addtional dialogs  ")
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+// VARIABLES FOR STRINGS | JAVASCRIPT chapter # 
+// 1. Declare a variable called username.
+var userName;
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+var myName = "Abdullah meer"
+
+document.write(myName)
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message = "Hello World"
+alert(message)
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+alert("Abdullah meer")
+alert(" 30 years old")
+alert("Certified Mobile Application Development ")
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+ var name = "PIZZA \n PIZZ \n PIZ \n PI \n P "
+ alert(name)
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var email = "My Email address is" + " " +  "abdullahmeer089@gmail.com"
+alert(email)
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book = "I am trying to learn from a book A smarter \n way to learn JavaScript”"
+alert(book)
+
+// 8. Write a script to display this in browser through JS
+document.write("Yah! I can write html content through JS ")
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+var image = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(image)
+
+var name = "MARKS SHEET" + "<br>" + "<br>"+ "<br>"+ "<br>"
+var mark1 = "Marks obtained"
+var mark = 804 +"<br>";
+var mark2 = "Total Marks"
+var totalMark = 980 +"<br>";
+var mark3= "percentage";
+var percentage = mark3+ 804* 100 / 980 +"<br>";
+ var sum = mark1 + mark+mark2+totalMark
+ document.write( name+sum + percentage)
